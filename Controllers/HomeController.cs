6094de61ce138252d9323156b335da32b3ec160c
Renato Rodrigues <luizrenatoporto@gmail.com>
2019-08-09 11:59:56 -0400

    Tela de Login

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoLogin.Controllers
+{
+    public class HomeController :  Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ViewResult Login(Login Logar)
+        {
+            return View("ConfirmarLogin", Logar);
+        }
+
+    }
+   
+}
+            
---
+++ Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoLogin.Models
+{
+    public class Login
+
+    {   [EmailAddress]
+        public string Email { get; set; }
+        [StringLength(3)]
+        public string Senha { get; set; }
+    }
+}
